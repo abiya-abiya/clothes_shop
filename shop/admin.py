804cddbc3aa2ad992056dacbf6bf79a8238cc24f
Abdulloh <anuraliyev71@gmail.com>
2026-08-11 11:22:03 +0500

    beshinchi vazifa

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from shop.models import Category, Product, Order
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+
+

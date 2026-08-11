af75303d639151e8927e143975a1827cd4dd47ef
Abdulloh <anuraliyev71@gmail.com>
2026-08-11 10:59:10 +0500

    uchinchi vazifa

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    SIZE_CHOICES = [
+        ("S", "S"),
+        ("M", "M"),
+        ("L", "L"),
+        ("XL", "XL"),
+    ]
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=5, choices=SIZE_CHOICES)
+    color = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="products/", blank=True)
+    description = models.TextField(blank=True)
+    is_available = models.BooleanField(default=True)
+
+class Order(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+    customer_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    quantity = models.PositiveBigIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer_name} --- {self.product.name}"

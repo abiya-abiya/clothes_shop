e1021b605eeb91590ba00fa50f3f5cdc6ede4fcb
Abdulloh <anuraliyev71@gmail.com>
2026-08-11 15:36:46 +0500

    yetinchi vazifa

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.get_home, name='home'),
-    path('products/', views.ProductListView.as_view(), name='product_list')
+    path('products/', views.ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name="product_detail"),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from shop.models import Product
@@ -13,3 +14,8 @@ class ProductListView(ListView):
     template_name = 'shop/product_list'
     context_object_name = 'products'
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "shop/product_detail.html"
+    context_object_name = "product"
+    pk_url_kwarg = 'pk'

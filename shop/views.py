e8482aaad8c2869f03a210f1c2cb55dfdfe12248
Abdulloh <anuraliyev71@gmail.com>
2026-08-11 16:06:37 +0500

    toqqizinchi vazifa

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
+
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 # Create your views here.
@@ -13,6 +14,23 @@ class ProductListView(ListView):
     model = Product
     template_name = 'shop/product_list'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get("category")
+
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['selected_category'] = self.request.GET.get('category')
+        return context
+
+
 
 class ProductDetailView(DetailView):
     model = Product
